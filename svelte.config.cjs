6ba3d756c8aad5053ff094ac453f3ae68693fc31
pranshu shah <pranshu.shah23@gmail.com>
2021-05-19 20:57:27 +0530

    netlify adapter added

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,6 +1,5 @@
 const sveltePreprocess = require('svelte-preprocess');
-const netlify = require('@sveltejs/adapter-static');
-
+const netlify = require('@sveltejs/adapter-netlify');
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
