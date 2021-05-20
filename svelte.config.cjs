18b854e9986f53b61deb63dd6bd88d58009ea3e4
pranshu shah <pranshu.shah23@gmail.com>
2021-05-20 11:53:21 +0530

    vercel changes -1

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const sveltePreprocess = require('svelte-preprocess');
-const netlify = require('@sveltejs/adapter-vercel');
+const vercel = require('@sveltejs/adapter-vercel');
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,7 +10,7 @@ module.exports = {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
-		adapter: netlify(),
+		adapter: vercel(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
