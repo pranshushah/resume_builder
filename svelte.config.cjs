056babb594652521af34833013f255ac7d40b665
pranshu shah <pranshu.shah23@gmail.com>
2021-05-20 11:27:16 +0530

    changed to static

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const sveltePreprocess = require('svelte-preprocess');
-const netlify = require('@sveltejs/adapter-netlify');
+const netlify = require('@sveltejs/adapter-static');
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
