db2dcddc8361dea2d677ea59576730c34c3df227
pranshu shah <pranshu.shah23@gmail.com>
2021-05-20 11:56:32 +0530

    packages added

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,7 @@
 const sveltePreprocess = require('svelte-preprocess');
 const vercel = require('@sveltejs/adapter-vercel');
+const pkg = require('./package.json');
+
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
